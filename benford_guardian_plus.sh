636b24e51466cded8e94ecdfef8322fca1ec78f5
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-03 18:14:52 -0300

    Create benford_guardian_plus.sh

---
+++ benford_guardian_plus.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Benford Guardian Plus - Installation and Usage Guide
+
+# Installation
+echo "=== Benford Guardian Plus - Installation ==="
+echo "1. Ensure that Python 3 is installed on your system."
+echo "2. Install the required dependencies:"
+echo "   $ pip install pandas matplotlib numpy scipy"
+echo "3. Download the 'benford_guardian_plus.py' script."
+echo "4. Make the script executable:"
+echo "   $ chmod +x benford_guardian_plus.py"
+echo "5. You're ready to use Benford Guardian Plus!"
+
+# Usage
+echo "=== Benford Guardian Plus - Usage ==="
+echo "To run the script, execute the following command:"
+echo "$ ./benford_guardian_plus.py network_traffic_data.csv"
+echo "Replace 'network_traffic_data.csv' with the path to your network traffic data file."
+echo "The script will analyze the data and provide insights on potential malicious behaviors."
+
+# End of instructions
